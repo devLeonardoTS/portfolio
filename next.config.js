dd71a971e1dbc252cd2eb1b220573dce990a58f2
Leonardo T. S <leonardo_takanobu@hotmail.com>
2022-12-15 20:25:39 -0300

    Fix: Deployment adjustments

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+const NEXT_APP_CONFIGS = require("./configs/NextAppConfig.json");
+
+function getNextAppConfig(env) {
+	switch (env) {
+		case "development":
+			return NEXT_APP_CONFIGS.envs.development;
+		case "test":
+			return NEXT_APP_CONFIGS.envs.test;
+		case "production":
+			return NEXT_APP_CONFIGS.envs.production;
+		default:
+			return NEXT_APP_CONFIGS.envs.development;
+	}
 }
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = getNextAppConfig(process.env.NODE_ENV);
+console.log(nextConfig);
+module.exports = nextConfig;
