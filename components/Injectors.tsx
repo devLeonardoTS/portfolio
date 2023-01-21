28a6cf5037d2187de2f62a1f991f1b4b73d0f0ca
Leonardo T. S <leonardo_takanobu@hotmail.com>
2023-01-21 14:08:04 -0300

    Add: Analytics

--- components/Injectors.tsx
+++ components/Injectors.tsx
@@ -1,6 +1,10 @@
 import { ReactNode } from "react";
 import { StyledEngineProvider } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import Script from "next/script";
+import AssetPaths from "../data/AssetPaths";
+import EnvVars from "../data/EnvVars";
 
 type InjectorsProps = {
 	children: ReactNode;
@@ -10,9 +14,28 @@ const queryClient = new QueryClient();
 
 const Injectors = ({ children }: InjectorsProps) => {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<StyledEngineProvider injectFirst>{children}</StyledEngineProvider>
-		</QueryClientProvider>
+		<React.Fragment>
+			{EnvVars.environment === "production" ? (
+				<React.Fragment>
+					<Script
+						id="google-analytics"
+						src="https://www.googletagmanager.com/gtag/js?id=G-0T2PQ0R322"
+						strategy="afterInteractive"
+					/>
+					<Script
+						id="google-analytics-tag-mgr"
+						src={AssetPaths.googleAnalyticsTagMgr}
+						strategy="afterInteractive"
+					/>
+				</React.Fragment>
+			) : null}
+
+			<QueryClientProvider client={queryClient}>
+				<StyledEngineProvider injectFirst>
+					{children}
+				</StyledEngineProvider>
+			</QueryClientProvider>
+		</React.Fragment>
 	);
 };
 
--- data/AssetPaths.ts
+++ data/AssetPaths.ts
@@ -4,6 +4,8 @@ import EnvVars from "./EnvVars";
 const AssetPaths = {
     base: EnvVars.basePath,
     imgBase: `${EnvVars.basePath}/images`,
+    scriptBase: `${EnvVars.basePath}/scripts`,
+    googleAnalyticsTagMgr: `${EnvVars.basePath}/scripts/google-analytics-tag-mgr.js`,
     favIcon: `${EnvVars.basePath}/favicon.ico`,
     slidePlaceholderImg: `${EnvVars.basePath}/images/ph-slide.webp`,
     accentProjectsDesign: `${EnvVars.basePath}/images/bg-projects-design.png`,
